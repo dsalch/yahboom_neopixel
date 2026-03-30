5629d17f9970d0403aab306537296b6b3d863566
David Salch <dsalch@salchfamily.org>
2026-03-30 02:32:46 -0500

    Update neotest.ts
    cleanup

--- neotest.ts
+++ neotest.ts
@@ -1,78 +1,12 @@
-{
-    let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB);
+// Initialise strip on P16 with 4 pixels (standard for many Yahboom cars)
+let strip = yahboom_lights.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
-    strip.showRainbow();
-    for (let i = 0; i <= strip.length(); i++) { 
-        strip.rotate();
-        strip.show();
-        basic.pause(100)
-    }
-    
-    strip.showColor(NeoPixelColors.Red)
-    basic.pause(2000)
-    strip.showColor(NeoPixelColors.Green)
-    basic.pause(1000)
-    for (let i = 0; i <= strip.length(); i++) {
-        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Blue))
-        strip.show()
-        basic.pause(100)
-    }
-    for (let i = 0; i <= strip.length(); i++) {
-        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Green))
-        strip.show()
-        basic.pause(100)
-    }
-    let sub = strip.range(10, 20)
-    sub.showColor(NeoPixelColors.Yellow);
-    basic.pause(200);
+// Test basic colors
+strip.showColor(NeoPixelColors.Red)
+basic.pause(500)
+strip.clear()
 
-    sub.showBarGraph(5, 10);
-    basic.pause(200);
-
-    let br = 100;
-    strip.setBrightness(100);
-    input.onButtonPressed(Button.B, () => {
-        br = br + 20;
-        if (br > 255) {
-            br = 5;
-        }
-        strip.setBrightness(br);
-    });
-
-    let rotationMode = false;
-    input.onButtonPressed(Button.A, () => {
-        rotationMode = !rotationMode;
-        if (rotationMode) {
-            basic.showLeds(`
-            . # # # .
-            # . . . #
-            # . . . #
-            # . . . #
-            . # # # .
-            `);
-        } else {
-            basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `);
-
-        }
-    });
-
-    while (true) {
-        let x = input.acceleration(Dimension.X) >> 1
-        let y = input.acceleration(Dimension.Y) >> 1
-        let z = input.acceleration(Dimension.Z) >> 1
-        if (rotationMode) {
-            strip.rotate();
-        } else {
-            strip.setPixelColor(0, neopixel.rgb(x, y, -z));
-            strip.shift(1);
-        }
-        strip.show();
-        basic.pause(100);
-    }
-}
+// Test individual pixel control
+strip.setPixelColor(0, NeoPixelColors.Blue)
+strip.setPixelColor(1, NeoPixelColors.Green)
+strip.show()
